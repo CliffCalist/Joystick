b962090d0c685bcb2c40877c020d2763faddae64
Cliff Calist <cliffstersprog@gmail.com>
2025-02-21 21:43:06 +0100

    init

---
+++ Runtime/FloatingJoystick.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.EventSystems;
+
+namespace WhiteArrow.UI
+{
+    public class FloatingJoystick : Joystick
+    {
+        protected override void Start()
+        {
+            background.gameObject.SetActive(false);
+            base.Start();
+        }
+
+        public override void OnPointerDown(PointerEventData eventData)
+        {
+            base.OnPointerDown(eventData);
+            background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
+            background.gameObject.SetActive(true);
+        }
+
+        public override void OnPointerUp(PointerEventData eventData)
+        {
+            base.OnPointerUp(eventData);
+            background.gameObject.SetActive(false);
+        }
+    }
+}
